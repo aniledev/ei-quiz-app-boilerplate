8e265b3ebd267a0641ccffc66604e61387af3153
aniledev <elinamcgill@gmail.com>
2020-11-18 12:22:00 -0500

    Added psuedocode to HTML generation, event handlers, and render functions

--- app.js
+++ app.js
@@ -85,10 +85,71 @@ const STORE = {
 
 // These functions return HTML templates
 
+function startScreen() {
+  // this function should simply return the start page HTML to the DOM
+}
+
+function showQuestion() {
+  // this function should trigger when the user clicks the start quiz button and it goes to the first question
+  // function needs to access question from the store variable
+  // function needs to access answers from the store variable
+  // function needs to return question, answers, info and submit button HTML to the DOM
+}
+
+function responseScreen() {
+  // this function should trigger when the user clicks the submit button to see if they got the question right or wrong
+  // function neds to access question from store variable
+  // function needs to acess answers from the store variable
+  // function needs to check if selected answer = correct answer
+  // if selected answer === correct answer return correct answer HTML to the dom
+  // if seleced answer !== correct answer reutnr incorrect answer HTML to the dom
+}
+
+function nextQuestion() {
+  // this function should trigger when the user clicks the next button and the next question should load
+  // this function needs to access the question from the STORE variable
+  // the function needs to access teh answer from the STORE variable
+  // this function needs to increment the index of the question in the STORE variable and return the HTML to the dom
+}
+
+function finalScreen() {
+  // this function shoulld trigger when the user hits the next button on the final question
+  // when question of last question === total numbers of questions (.length)
+  // if true return the final screen HTML to the DOM
+  // else generate next question and return the HTML to the DOM
+}
+
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
+function renderStartScreen() {
+  // this function will render the start screen depending on if the quiz === true or false
+  // if quiz started === false then render the homepage / call the start screen function
+  // if quiz started === true then render the question screen/ call the question screen function
+  // this should initially render the start screen, no questions
+}
+
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+function main() {
+  // this is the main function for the page that will fire when the page loads
+  // renderStartScreen();
+  // handleStartQuiz();
+}
+
+function handleStartQuiz() {
+  // this function should trigger when then the start quiz button is clicked. once clicked it should move user to the question
+  // create a click event on the start button
+  // update the STORE variable for the start quiz === true
+  // trigger the render function for start screen
+}
+
+function tryAgain() {
+  // this function should trigger when the user clicks the try again button and they should be directed to the start of the screen
+  // create click event of try agian button
+  // change the start quiz variable to false
+  // render the start screen HTML
+}
